65cd2bac7fe7a361123af09694666192995ab6cb
Kidist Seifu Gragne <kidistsg@1x-193-157-214-87.uio.no>
2016-04-06 13:16:11 +0200

    Use variable WORK_D and PATH to hold plist and /Library/LaunchAgent paths

--- launch-msc-then-destruct.sh
+++ launch-msc-then-destruct.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
+WORK_D=no.uio.usit.launch-msc.plist
+PATH=/Library/LaunchAgent
+
 open /Applications/Managed\ Software\ Center.app
-launchctl stop com.myorg.connectshare.plist
-launchctl unload /Library/LaunchAgents/com.myorg.connectshare.plist
-rm /Library/LaunchAgents/com.myorg.connectshare.plist
+launchctl stop ${WORK_D}
+launchctl unload ${PATH}/${WORK_D}
+rm ${PATH}/${WORK_D}
 srm "$0"
